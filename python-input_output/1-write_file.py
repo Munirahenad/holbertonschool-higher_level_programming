adcf3246b9a6f231410ec12d9c6ed609e4bfcb2c
munirahenad <muneraenad@hotmail.com>
2026-02-01 07:53:33 +0000

    Fix 1-write_file.py

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,6 @@ Contains function write_file that writes a string to a UTF-8 text file.
 
 
 def write_file(filename="", text=""):
-    """Writes a string to a text file (UTF-8) and returns the number of characters written."""
+    """Write a string to a UTF-8 text file and return chars count."""
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
