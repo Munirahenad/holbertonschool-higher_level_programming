201658f302f04c7dc2092a538aa798ea2c261d2e
munirahenad <muneraenad@hotmail.com>
2026-02-01 07:25:03 +0000

    Add read_file function with module docstring

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""Module 0-read_file
+Contains function read_file that reads a UTF-8 text file and prints to stdout.
+"""
+
+
 def read_file(filename=""):
     """Reads a text file (UTF-8) and prints it to stdout."""
     with open(filename, "r", encoding="utf-8") as f:
