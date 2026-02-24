47ce57b3f0a5a8f6cfecfa998b85bc721da24fcd
munirahenad <muneraenad@hotmail.com>
2026-02-24 09:39:03 +0000

    update

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Module that contains the City class definition"""
-from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+"""Module that contains the State class and Base instance"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class City(Base):
-    """City class that links to the MySQL table cities"""
-    __tablename__ = 'cities'
+class State(Base):
+    """State class that links to the MySQL table states"""
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
